635f710f81cf9a6ec8c1b2ed630f71103d61a2be
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-04-02 00:37:53 -0400

    now we have the Event class.

--- reporter.rb
+++ reporter.rb
@@ -1,8 +1,48 @@
+require 'digest'
+
+class Event
+  attr_reader :id
+
+  def initialize
+    @id = Digest::MD5.hexdigest(self.object_id.to_s)
+    @done = false
+  end
+
+  def finish
+    @done = true
+  end
+
+  def done?
+    @done
+  end
+end
+
 class Detector
   def unfinished_events(start_events, end_events)
     start_events.reject do |start_event|
       end_events.any? { |event| start_event.id == event.id }
     end
+  end
+end
+
+describe Event do
+  it "has a unique id" do
+    events = []
+    50.times do
+      events << Event.new
+    end
+    events_ids = events.map(&:id).uniq
+    events_ids.count.should == events.count
+  end
+
+  it "is not done at creation" do
+    Event.new.should_not be_done
+  end
+
+  it "can be finished" do
+    event = Event.new
+    event.finish
+    event.should be_done
   end
 end
 
