7ebd6a52f1ea54455d19a5502a0d110990066ad7
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-04-02 01:14:53 -0400

    Store the events (start and end).

--- reporter.rb
+++ reporter.rb
@@ -4,12 +4,14 @@ class Event
   attr_reader :id
 
   def initialize
-    @id = Digest::MD5.hexdigest(self.object_id.to_s)
+    @id = Digest::MD5.hexdigest self.object_id.to_s
     @done = false
+    Storage::DB.save_start_event self
   end
 
   def finish
     @done = true
+    Storage::DB.save_end_event self
   end
 
   def done?
@@ -25,55 +27,31 @@ class Detector
   end
 end
 
-describe Event do
-  it "has a unique id" do
-    events = []
-    50.times do
-      events << Event.new
+module Storage
+  class DB
+    def self.start_events
+      @@start_events
     end
-    events_ids = events.map(&:id).uniq
-    events_ids.count.should == events.count
-  end
 
-  it "is not done at creation" do
-    Event.new.should_not be_done
-  end
-
-  it "can be finished" do
-    event = Event.new
-    event.finish
-    event.should be_done
-  end
-end
-
-describe Detector do
-  let(:detector) { Detector.new }
-
-  describe "no events at all" do
-    it "returns an empty collection" do
-      detector.unfinished_events([], []).should == []
+    def self.end_events
+      @@end_events
     end
-  end
 
-  describe "all events are finished" do
-    it "returns empty collection when all ids match" do
-      event1 = stub(id: 1)
-      event2 = stub(id: 2)
-      start_events = [event1, event2]
-      end_events = [event1, event2]
-
-      detector.unfinished_events(start_events, end_events).should == []
+    def self.save_start_event event
+      @@start_events ||= []
+      @@start_events << event
     end
-  end
 
-  describe "some events are not finished" do
-    it "returns unfinished events" do
-      event1 = stub(id: 1)
-      event2 = stub(id: 2)
-      start_events = [event1, event2]
-      end_events = [event1, stub(id: 3)]
+    def self.save_end_event event
+      @@end_events ||= []
+      @@end_events << event
+    end
 
-      detector.unfinished_events(start_events, end_events).should == [event2]
+    def self.clear_data
+      @@start_events ||= []
+      @@end_events ||= []
+      @@start_events.clear 
+      @@end_events.clear 
     end
   end
 end
